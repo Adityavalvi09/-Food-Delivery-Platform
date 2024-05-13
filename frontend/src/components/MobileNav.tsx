b698212467f9872a8307b686bd5adbad7b4747fc
Aditya Valvi <adityavalvi8392@gmail.com>
2024-05-13 16:14:55 +0530

    first commit

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -3,6 +3,7 @@ import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from 
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = ()=>{
     const {isAuthenticated , loginWithRedirect, user} = useAuth0();
@@ -23,8 +24,8 @@ const MobileNav = ()=>{
                  )}
                 </SheetTitle>
                 <Separator>
-                    <SheetDescription className="flex">
-                        {isAuthenticated ? (<MobileNavLinks/> ):(  <Button className="flex-1 font-bold bg-orange-500">Log In </Button>}
+                    <SheetDescription className="flex flex-col gap-4">
+                        {isAuthenticated ? (<MobileNavLinks/> ):(  <Button onClick= {()=> loginWithRedirect}className="flex-1 font-bold bg-orange-500">Log In </Button>)}
                        
                     </SheetDescription>
                 </Separator>
--- frontend/src/components/MobileNavLinks.tsx
+++ frontend/src/components/MobileNavLinks.tsx
@@ -1,1 +1,19 @@
- 
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
+ const MobileNavLinks = () => {
+    return (
+        <>
+         <Link to="/user-profile" className="flex bg-white items-center font-bold hover:text-orange-500">
+            User Profile
+         </Link>
+
+         <Button className="flex items-center px-3 font ">
+            Log Out
+
+         </Button>
+        </>
+    )
+ }
+
+ export default MobileNavLinks;
