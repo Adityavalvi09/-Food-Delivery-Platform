ca57c23b71635289b90f53abd9cf0908a289e94f
Aditya Valvi <adityavalvi8392@gmail.com>
2024-05-30 21:49:14 +0530

    first commit

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -1,9 +1,14 @@
+import { error } from "console";
 import {Request,Response, NextFunction } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 
 
 const handleValidationErrors = async(req:Request, res:Response , next:NextFunction)=>{
-
+      const errors = validationResult(req);
+      if(!errors.isEmpty()){
+        return res.status(400).json({error: errors.array()})
+      }
+      next();
 }
 export const validateMyUserRequest = [
     body ("name").isString().notEmpty().withMessage("Name must be a string"),
@@ -13,5 +18,5 @@ export const validateMyUserRequest = [
     .withMessage("Address Must be string"),
 body("city").isString().notEmpty().withMessage("city must be string"),
 body("country").isString().notEmpty().withMessage("Country must be string"),
-
+handleValidationErrors,
 ];
